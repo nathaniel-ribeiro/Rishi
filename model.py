02d6cc235bf9d5b2a8a8e95cbfe97d9ad2aec7d3
Nathaniel Ribeiro <cwa6zf@virginia.edu>
2025-11-04 19:25:57 -0500

    reinitialize weights as recommended in PyTorch documentation for TransformerEncoder

--- model.py
+++ model.py
@@ -23,7 +23,8 @@ class SinusoidalPositionalEncoding(nn.Module):
 class TransformerClassifier(nn.Module):
     def __init__(self, vocab_size, max_seq_len, d_model=256, n_layers=8, n_heads=8, dropout=0.1):
         super(TransformerClassifier, self).__init__()
-        self.token_embedding = nn.Embedding(vocab_size, d_model)
+        self.vocab_size = vocab_size
+        self.token_embedding = nn.Embedding(self.vocab_size, d_model)
         self.pos_encoding = SinusoidalPositionalEncoding(d_model, max_seq_len)
 
         encoder_layer = nn.TransformerEncoderLayer(
@@ -39,6 +40,22 @@ class TransformerClassifier(nn.Module):
             nn.LayerNorm(d_model),
             nn.Linear(d_model, 1)
         )
+
+        self.apply(self._init_weights)
+    
+    def _init_weights(self, module):
+        # re-initialize since TransformerEncoder initializes all layers with the same weights
+        if isinstance(module, nn.Linear):
+            nn.init.xavier_uniform_(module.weight)
+            if module.bias is not None:
+                nn.init.constant_(module.bias, 0)
+            
+        elif isinstance(module, nn.LayerNorm):
+            nn.init.constant_(module.weight, 1.0)
+            nn.init.constant_(module.bias, 0)
+        
+        elif isinstance(module, nn.Embedding):
+            nn.init.normal_(module.weight, mean=0, std=torch.sqrt(2.0 / (module.embedding_dim + self.vocab_size)))
     
     def forward(self, x):
         x = self.token_embedding(x)
